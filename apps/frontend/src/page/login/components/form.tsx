173be66a3b4907fefcd41b24bd23a65960a957ce
yuransimao <yuransimao7@gmail.com>
2024-11-10 18:36:16 +0100

    chore:added components for login

---
+++ apps/frontend/src/page/login/components/form.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Input, Button} from "@/components"
+import { LogIn } from 'lucide-react'
+
+function Form() {
+  return (
+    <React.Fragment>
+        <form  className='space-y-4'>
+        <Input type="email" placeholder="Email" className='p-6 rounded-md'/>
+        <Input type="password" placeholder="Password"  className='p-6 rounded-md'/>
+        <Button variant='outline' className='rounded-md'>
+            <span><LogIn/></span>
+            <h3>Entrar</h3>
+        </Button>
+        </form>
+    </React.Fragment>
+  )
+}
+
+export {Form}
